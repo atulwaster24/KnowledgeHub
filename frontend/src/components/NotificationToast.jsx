ef2a8d273837f552ee1c67f935bc7e2f00782b2e
atulwaster24 <atulwasterstar@gmail.com>
2026-01-10 13:16:01 +0530

    frontend-feature: setup and auth for client side

---
+++ frontend/src/components/NotificationToast.jsx
@@ -0,0 +1,7 @@
+export default function NotificationToast ({message}) {
+    return (
+        <div className="fixed top-4 right-4 bg-black text-white px-4 py-2 rounded shadow">
+            {message}
+        </div>
+    )
+}
